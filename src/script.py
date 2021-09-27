8e55a5206a83110cc6523673f84fddbf787aee42
Sergiu-Daniel Petrache <sergi.petrache@gmail.com>
2021-09-27 10:30:23 +0200

    Encriptar lletres amb accents

--- src/script.py
+++ src/script.py
@@ -1,5 +1,9 @@
 import os.path
 import string
+import sys
+import unicodedata
+
+
 
 # Encriptació a través d'un generador congruencial
 
@@ -13,16 +17,29 @@ alphabet_list = list(alphabet_string)
 alphabet_string_up = string.ascii_uppercase
 alphabet_list_up = list(alphabet_string_up)
 
+# Elimina tots els accents de el text entrat i retorna una cadena de caràcters amb el text processat
+def normalitzar(text):
+    try:
+        text = unicode(text, 'utf-8')
+    except (TypeError, NameError): # unicode is a default on python 3 
+        pass
+    text = unicodedata.normalize('NFD', text)
+    text = text.encode('ascii', 'ignore')
+    text = text.decode("utf-8")
+    return str(text)
+
 # Funció que encripta una línia i la retorna en forma de cadena de caràcters
 def encriptar_linia(linia, a, c):
 
     linia_encriptada = ""
     for caracter in range(0, len(linia)):
         if(linia[caracter].isupper()):
-            resultat = ((a + alphabet_list_up.index(linia[caracter])) * c) % 26
+            caracter_normal = normalitzar(linia[caracter])
+            resultat = ((a + alphabet_list_up.index(caracter_normal)) * c) % 26
             linia_encriptada = linia_encriptada + alphabet_list_up[resultat]
         elif(linia[caracter].islower()):
-            resultat = ((a + alphabet_list.index(linia[caracter])) * c) % 26
+            caracter_normal = normalitzar(linia[caracter])
+            resultat = ((a + alphabet_list.index(caracter_normal)) * c) % 26
             linia_encriptada = linia_encriptada + alphabet_list[resultat]
         else:
             linia_encriptada = linia_encriptada + linia[caracter]
@@ -43,6 +60,19 @@ def desencriptar_linia(linia, a, c):
             linia_desencriptada = linia_desencriptada + linia[caracter]
     return linia_desencriptada
 
+# Funció que a partir d'una línia, treus els accents de totes les lletres que en porten, i retorna una cadena de caràcters amb aquests sense accents
+def normalitzar_linia(linia):
+    linia_normalitzada = ""
+    for caracter in range(0, len(linia)):
+        if(linia[caracter].isupper()):
+            caracter_normal = normalitzar(linia[caracter])
+            linia_normalitzada = linia_normalitzada + caracter_normal
+        elif(linia[caracter].islower()):
+            caracter_normal = normalitzar(linia[caracter])
+            linia_normalitzada = linia_normalitzada + caracter_normal
+        else:
+            linia_normalitzada = linia_normalitzada + linia[caracter]
+    return linia_normalitzada
 
 
 # Funció principal
@@ -54,7 +84,7 @@ def main():
     a = 2 # a = input("Coeficient a: ")
     c = 3 # c = input("Coeficient c: ")
     # s = input("Llavor s: ")
-    nomFitxer = "fitxer1.txt" #input("Nom fitxer: ")
+    nomFitxer = "fitxer2.txt" #input("Nom fitxer: ")
     nomFitxer_xifrat = "fitxer_xifrat.txt"
     nomFitxer_desxifrat = "fitxer_desxifrat.txt"
 
@@ -67,14 +97,19 @@ def main():
         quit()
 
     # Llegim el fitxer i obriem el fitxer amb el text encriptat
-    fitxer = open(ruta, 'r')
+    fitxer = open(ruta, 'r', encoding='utf-8')
     fitxer_xifrat = open(ruta_xifrat, 'w')
     linies = fitxer.readlines()
+
+    fn = open('normalitzat.txt', 'w')
+
+    for linia in linies:
+        linia_normal = normalitzar_linia(linia)
+        fn.write(linia_normal)
 
     # Encriptem el text normal del fitxer i el disposem dins d'un segon fitxer
     for linia in linies:
         linia_encriptada = encriptar_linia(linia, a, c)
-        print(linia_encriptada)
         fitxer_xifrat.write(linia_encriptada)
 
     fitxer_desxifrar = open(ruta_xifrat, 'r')
@@ -91,6 +126,7 @@ def main():
     fitxer_xifrat.close()
     fitxer_desxifrar.close()
     fitxer_desxifrat.close()
+    fn.close()
 
 if __name__ == '__main__':
     main()
